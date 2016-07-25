087e31dd559e01cde90697d58fa311f2a78c1bb0
Hokun Baek <hokun.baek@gmail.com>
2016-07-25 14:28:45 +0900

    add variables

---
+++ 003_Variables/variables.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a string = "init"
+	fmt.Println(a)
+
+	b := "init"
+	fmt.Println(b)
+
+	var c, d int = 1, 2
+	fmt.Printf("c = %d, d = %d\n", c, d)
+
+	var e bool = true
+	fmt.Println(e)
+}
