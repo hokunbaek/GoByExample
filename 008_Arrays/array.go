442cb9961370fe683b1718f43c2b27792c033e75
Hokun Baek <hokun.baek@gmail.com>
2016-07-27 14:00:13 +0900

    add Arrays

---
+++ 008_Arrays/array.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a [5]int
+	fmt.Println(a)
+
+	a[0] = 100
+	fmt.Println(a)
+
+	fmt.Println(len(a))
+
+	var twoD [2][3]int
+	fmt.Println(twoD)
+
+	for i := 0; i < 2; i++ {
+		for j := 0; j < 3; j++ {
+			twoD[i][j] = i + j
+		}
+	}
+	fmt.Println(twoD)
+}
